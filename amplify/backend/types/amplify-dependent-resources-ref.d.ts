3b101db8aae43d27209612277506c51fb4fc2209
EC2 Default User <ec2-user@ip-10-1-0-37.ec2.internal>
2022-08-26 11:26:12 +0000

    added auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,12 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "vendorapp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
     }
 }
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,120 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateVendor = /* GraphQL */ `
+  subscription OnCreateVendor {
+    onCreateVendor {
+      id
+      requestnumber
+      requestedoperation
+      status
+      vendorname
+      vendorfirstname
+      vendorfullname
+      addressline1
+      postalcode
+      cityname
+      country
+      region
+      emailaddress
+      languagekey
+      thirdpartysupplieraccgrp
+      purchasingorg
+      pocurrency
+      paymentkeyterms
+      incotermspart1
+      purchasinggroup
+      planneddeliverytimedays
+      companycode
+      reconcilliationaccount
+      streetname
+      blockkey4payment
+      industrykey
+      vendortype
+      customernumber
+      contacttitle
+      vendorgrouping
+      image
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateVendor = /* GraphQL */ `
+  subscription OnUpdateVendor {
+    onUpdateVendor {
+      id
+      requestnumber
+      requestedoperation
+      status
+      vendorname
+      vendorfirstname
+      vendorfullname
+      addressline1
+      postalcode
+      cityname
+      country
+      region
+      emailaddress
+      languagekey
+      thirdpartysupplieraccgrp
+      purchasingorg
+      pocurrency
+      paymentkeyterms
+      incotermspart1
+      purchasinggroup
+      planneddeliverytimedays
+      companycode
+      reconcilliationaccount
+      streetname
+      blockkey4payment
+      industrykey
+      vendortype
+      customernumber
+      contacttitle
+      vendorgrouping
+      image
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteVendor = /* GraphQL */ `
+  subscription OnDeleteVendor {
+    onDeleteVendor {
+      id
+      requestnumber
+      requestedoperation
+      status
+      vendorname
+      vendorfirstname
+      vendorfullname
+      addressline1
+      postalcode
+      cityname
+      country
+      region
+      emailaddress
+      languagekey
+      thirdpartysupplieraccgrp
+      purchasingorg
+      pocurrency
+      paymentkeyterms
+      incotermspart1
+      purchasinggroup
+      planneddeliverytimedays
+      companycode
+      reconcilliationaccount
+      streetname
+      blockkey4payment
+      industrykey
+      vendortype
+      customernumber
+      contacttitle
+      vendorgrouping
+      image
+      createdAt
+      updatedAt
+    }
+  }
+`;
