5887ff4dd4514e016a8da511c63b51c8e322e92a
EC2 Default User <ec2-user@ip-10-1-0-37.ec2.internal>
2022-09-02 10:46:02 +0000

    me

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-        <h2>Shaista Reyaz</h2>
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <h1>Hello from V2</h1>
+//         <h2>Shaista Reyaz</h2>
         
         
-      </header>
-    </div>
-  );
-}
+//       </header>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
 
 // import React, { useState, useEffect } from 'react';
 // import './App.css';
@@ -252,228 +252,230 @@ export default App;
 // export default withAuthenticator(App);
 
 
-// import React, { useState, useEffect } from 'react';
-// import './App.css';
-// import { API } from 'aws-amplify';
-// import { withAuthenticator, Authenticator } from '@aws-amplify/ui-react';
-// import { listVendors } from './graphql/queries';
-// import { createVendor as createVendorMutation, deleteVendor as deleteVendorMutation } from './graphql/mutations';
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import { API } from 'aws-amplify';
+import { withAuthenticator, Authenticator } from '@aws-amplify/ui-react';
+import { listVendors } from './graphql/queries';
+import { createVendor as createVendorMutation, deleteVendor as deleteVendorMutation } from './graphql/mutations';
+// import { DataStore } from '@aws-amplify/datastore';
+// import { Vendor } from './models';
 
-// const initialFormState = { id: '' ,requestnumber: '' ,requestedoperation: '' ,status: '' ,vendorname: '' ,vendorfirstname: '' ,vendorfullname: '' ,addressline1: '' ,postalcode: '' ,cityname: '' ,country: '' ,region: '' ,emailaddress: '' ,languagekey: '' ,thirdpartysupplieraccgrp: '' ,purchasingorg: '' ,pocurrency: '' ,paymentkeyterms: '' ,incotermspart1: '' ,purchasinggroup: '' ,planneddeliverytimedays: '' ,companycode: '' ,reconcilliationaccount: '' ,streetname: '' ,blockkey4payment: '' ,industrykey: '' ,vendortype: '' ,customernumber: '' ,contacttitle: '' ,vendorgrouping: '' }
-// function App() {
-// const [vendors, setVendors] = useState([]);
-//   const [formData, setFormData] = useState(initialFormState);
+const initialFormState = { id: '' ,requestnumber: '' ,requestedoperation: '' ,status: '' ,vendorname: '' ,vendorfirstname: '' ,vendorfullname: '' ,addressline1: '' ,postalcode: '' ,cityname: '' ,country: '' ,region: '' ,emailaddress: '' ,languagekey: '' ,thirdpartysupplieraccgrp: '' ,purchasingorg: '' ,pocurrency: '' ,paymentkeyterms: '' ,incotermspart1: '' ,purchasinggroup: '' ,planneddeliverytimedays: '' ,companycode: '' ,reconcilliationaccount: '' ,streetname: '' ,blockkey4payment: '' ,industrykey: '' ,vendortype: '' ,customernumber: '' ,contacttitle: '' ,vendorgrouping: '' }
+function App() {
+const [vendors, setVendors] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
 
-//   useEffect(() => {
-//     fetchVendors();
-//   }, []);
+  useEffect(() => {
+    fetchVendors();
+  }, []);
 
-//   async function fetchVendors() {
-//     const apiData = await API.graphql({ query: listVendors });
-//     setVendors(apiData.data.listVendors.items);
-//   }
+  async function fetchVendors() {
+    const apiData = await API.graphql({ query: listVendors });
+    setVendors(apiData.data.listVendors.items);
+  }
 
-//   async function createVendor() {
-//     console.log("createvendor called : " + JSON.stringify(formData));
-//     //if (!formData.id || !formData.vendorname || !formData.requestnumber) return;
-//     console.log("all set to create vendor");
-//     await API.graphql({ query: createVendorMutation, variables: { input: formData } });
-//     console.log("create vendor done");
+  async function createVendor() {
+    console.log("createvendor called : " + JSON.stringify(formData));
+    //if (!formData.id || !formData.vendorname || !formData.requestnumber) return;
+    console.log("all set to create vendor");
+    await API.graphql({ query: createVendorMutation, variables: { input: formData } });
+    console.log("create vendor done");
 
-//     setVendors([ ...vendors, formData ]);
-//     setFormData(initialFormState);
-//   }
+    setVendors([ ...vendors, formData ]);
+    setFormData(initialFormState);
+  }
 
-//   async function deleteVendor({ id }) {
-//     const newVendorsArray = vendors.filter(vendor => vendor.id !== id);
-//     setVendors(newVendorsArray);
-//     await API.graphql({ query: deleteVendorMutation, variables: { input: { id } }});
-//   }
+  async function deleteVendor({ id }) {
+    const newVendorsArray = vendors.filter(vendor => vendor.id !== id);
+    setVendors(newVendorsArray);
+    await API.graphql({ query: deleteVendorMutation, variables: { input: { id } }});
+  }
 
-//   return (
-//     <div className="App">
-//       <h1>My Vendors App</h1>
-//       <input
-//         onChange={e => setFormData({ ...formData, 'id': e.target.value})}
-//         placeholder="id"
-//         value={formData.id}
-//       />
-//       <input
-//         onChange={e => setFormData({ ...formData, 'requestnumber': e.target.value})}
-//         placeholder="Requested Number"
-//         value={formData.requestnumber}
-//       />
-//       <input
-//         onChange={e => setFormData({ ...formData, 'requestedoperation': e.target.value})}
-//         placeholder="Requested Operation"
-//         value={formData.requestedoperation}
-//       />
-//       <input
-//         onChange={e => setFormData({ ...formData, 'status': e.target.value})}
-//         placeholder="status"
-//         value={formData.status}
-//       />
-//       <input
-//         onChange={e => setFormData({ ...formData, 'vendorname': e.target.value})}
-//         placeholder="vendorname"
-//         value={formData.vendorname}
-//       />
-//       <input
-//         onChange={e => setFormData({ ...formData, 'vendorfirstname': e.target.value})}
-//         placeholder="vendorfirstname"
-//         value={formData.vendorfirstname}
-//       />
-//       <input
-//         onChange={e => setFormData({ ...formData, 'vendorfullname': e.target.value})}
-//         placeholder="vendorfullname"
-//         value={formData.vendorfullname}
-//       />
-//       <input
-//         onChange={e => setFormData({ ...formData, 'addressline1': e.target.value})}
-//         placeholder="addressline1"
-//         value={formData.addressline1}
-//       />
-//       <input
-//         onChange={e => setFormData({ ...formData, 'postalcode': e.target.value})}
-//         placeholder="postalcode"
-//         value={formData.postalcode}
-//       />
-//       <input
-//         onChange={e => setFormData({ ...formData, 'cityname': e.target.value})}
-//         placeholder="cityname"
-//         value={formData.cityname}
-//       />
-//       <input
-//         onChange={e => setFormData({ ...formData, 'country': e.target.value})}
-//         placeholder="country"
-//         value={formData.country}
-//       />
-//       <input
-//         onChange={e => setFormData({ ...formData, 'region': e.target.value})}
-//         placeholder="region"
-//         value={formData.region}
-//       />
-//       <input
-//         onChange={e => setFormData({ ...formData, 'emailaddress': e.target.value})}
-//         placeholder="emailaddress"
-//         value={formData.emailaddress}
-//       />
-//       <input
-//         onChange={e => setFormData({ ...formData, 'languagekey': e.target.value})}
-//         placeholder="languagekey"
-//         value={formData.languagekey}
-//       />
-//       <input
-//       onChange={e => setFormData({ ...formData, 'thirdpartysupplieraccgrp': e.target.value})}
-//         placeholder="thirdpartysupplieraccgrp"
-//         value={formData.thirdpartysupplieraccgrp}
-//       />
-//       <input
-//         onChange={e => setFormData({ ...formData, 'purchasingorg': e.target.value})}
-//         placeholder="purchasingorg"
-//         value={formData.purchasingorg}
-//       />
-//       <input
-//         onChange={e => setFormData({ ...formData, 'pocurrency': e.target.value})}
-//         placeholder="pocurrency"
-//         value={formData.pocurrency}
-//       />
-//       <input
-//         onChange={e => setFormData({ ...formData, 'paymentkeyterms': e.target.value})}
-//         placeholder="paymentkeyterms"
-//         value={formData.paymentkeyterms}
-//       />
-//       <input
-//         onChange={e => setFormData({ ...formData, 'incotermspart1': e.target.value})}
-//         placeholder="incotermspart1"
-//         value={formData.incotermspart1}
-//       />
-//       <input
-//         onChange={e => setFormData({ ...formData, 'purchasinggroup': e.target.value})}
-//         placeholder="purchasinggroup"
-//         value={formData.purchasinggroup}
-//       />
-//       <input
-//         onChange={e => setFormData({ ...formData, 'planneddeliverytimedays': e.target.value})}
-//         placeholder="planneddeliverytimedays"
-//         value={formData.planneddeliverytimedays}
-//       />
-//       <input
-//         onChange={e => setFormData({ ...formData, 'companycode': e.target.value})}
-//         placeholder="companycode"
-//         value={formData.companycode}
-//       />
-//       <input
-//         onChange={e => setFormData({ ...formData, 'reconcilliationaccount': e.target.value})}
-//         placeholder="reconcilliationaccount"
-//         value={formData.reconcilliationaccount}
-//       />
-//       <input
-//         onChange={e => setFormData({ ...formData, 'streetname': e.target.value})}
-//         placeholder="streetname"
-//         value={formData.streetname}
-//       />
-//       <input
-//         onChange={e => setFormData({ ...formData, 'blockkey4payment': e.target.value})}
-//         placeholder="blockkey4payment"
-//         value={formData.blockkey4payment}
-//       />
-//       <input
-//         onChange={e => setFormData({ ...formData, 'industrykey': e.target.value})}
-//         placeholder="industrykey"
-//         value={formData.industrykey}
-//       />
-//       <input
-//         onChange={e => setFormData({ ...formData, 'vendortype': e.target.value})}
-//         placeholder="vendortype"
-//         value={formData.vendortype}
-//       />
-//       <input
-//         onChange={e => setFormData({ ...formData, 'customernumber': e.target.value})}
-//         placeholder="customernumber"
-//         value={formData.customernumber}
-//       />
-//       <input
-//         onChange={e => setFormData({ ...formData, 'contacttitle': e.target.value})}
-//         placeholder="contacttitle"
-//         value={formData.contacttitle}
-//       />
-//       <input
-//         onChange={e => setFormData({ ...formData, 'vendorgrouping': e.target.value})}
-//         placeholder="vendorgrouping"
-//         value={formData.vendorgrouping}
-//       />
-//       <button onClick={createVendor}>Create Vendor</button>
-//       <div style={{marginBottom: 30}}>
-//         {
-//           vendors.map(vendor => (
+  return (
+    <div className="App">
+      <h1>My Vendors App</h1>
+      <input
+        onChange={e => setFormData({ ...formData, 'id': e.target.value})}
+        placeholder="id"
+        value={formData.id}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'requestnumber': e.target.value})}
+        placeholder="Requested Number"
+        value={formData.requestnumber}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'requestedoperation': e.target.value})}
+        placeholder="Requested Operation"
+        value={formData.requestedoperation}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'status': e.target.value})}
+        placeholder="status"
+        value={formData.status}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'vendorname': e.target.value})}
+        placeholder="vendorname"
+        value={formData.vendorname}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'vendorfirstname': e.target.value})}
+        placeholder="vendorfirstname"
+        value={formData.vendorfirstname}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'vendorfullname': e.target.value})}
+        placeholder="vendorfullname"
+        value={formData.vendorfullname}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'addressline1': e.target.value})}
+        placeholder="addressline1"
+        value={formData.addressline1}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'postalcode': e.target.value})}
+        placeholder="postalcode"
+        value={formData.postalcode}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'cityname': e.target.value})}
+        placeholder="cityname"
+        value={formData.cityname}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'country': e.target.value})}
+        placeholder="country"
+        value={formData.country}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'region': e.target.value})}
+        placeholder="region"
+        value={formData.region}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'emailaddress': e.target.value})}
+        placeholder="emailaddress"
+        value={formData.emailaddress}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'languagekey': e.target.value})}
+        placeholder="languagekey"
+        value={formData.languagekey}
+      />
+      <input
+      onChange={e => setFormData({ ...formData, 'thirdpartysupplieraccgrp': e.target.value})}
+        placeholder="thirdpartysupplieraccgrp"
+        value={formData.thirdpartysupplieraccgrp}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'purchasingorg': e.target.value})}
+        placeholder="purchasingorg"
+        value={formData.purchasingorg}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'pocurrency': e.target.value})}
+        placeholder="pocurrency"
+        value={formData.pocurrency}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'paymentkeyterms': e.target.value})}
+        placeholder="paymentkeyterms"
+        value={formData.paymentkeyterms}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'incotermspart1': e.target.value})}
+        placeholder="incotermspart1"
+        value={formData.incotermspart1}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'purchasinggroup': e.target.value})}
+        placeholder="purchasinggroup"
+        value={formData.purchasinggroup}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'planneddeliverytimedays': e.target.value})}
+        placeholder="planneddeliverytimedays"
+        value={formData.planneddeliverytimedays}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'companycode': e.target.value})}
+        placeholder="companycode"
+        value={formData.companycode}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'reconcilliationaccount': e.target.value})}
+        placeholder="reconcilliationaccount"
+        value={formData.reconcilliationaccount}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'streetname': e.target.value})}
+        placeholder="streetname"
+        value={formData.streetname}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'blockkey4payment': e.target.value})}
+        placeholder="blockkey4payment"
+        value={formData.blockkey4payment}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'industrykey': e.target.value})}
+        placeholder="industrykey"
+        value={formData.industrykey}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'vendortype': e.target.value})}
+        placeholder="vendortype"
+        value={formData.vendortype}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'customernumber': e.target.value})}
+        placeholder="customernumber"
+        value={formData.customernumber}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'contacttitle': e.target.value})}
+        placeholder="contacttitle"
+        value={formData.contacttitle}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'vendorgrouping': e.target.value})}
+        placeholder="vendorgrouping"
+        value={formData.vendorgrouping}
+      />
+      <button onClick={createVendor}>Create Vendor</button>
+      <div style={{marginBottom: 30}}>
+        {
+          vendors.map(vendor => (
           
-//             <div key={vendor.id || vendor.vendorname}>
-//               <h2>{vendor.vendorname}bcnbm n</h2>
-//               <p>{vendor.status}</p>
-//               <p>{vendor.emailaddress}</p>
-//               <p>{vendor.addressline1}</p>
+            <div key={vendor.id || vendor.vendorname}>
+              <h2>{vendor.vendorname}bcnbm n</h2>
+              <p>{vendor.status}</p>
+              <p>{vendor.emailaddress}</p>
+              <p>{vendor.addressline1}</p>
               
-//               <button onClick={() => deleteVendor(vendor)}>Delete vendor</button>
-//             </div>
-//           ))
+              <button onClick={() => deleteVendor(vendor)}>Delete vendor</button>
+            </div>
+          ))
            
-//         }
+        }
        
-//       </div>
-//       <Authenticator>
-//                               {({ signOut, user }) => (
-//                                           <main>
-//                                             <h1>Hello {user.username}</h1>
-//                                             <button onClick={signOut}>Sign out</button>
-//                                           </main>
-//                               )} 
-//                             </Authenticator>
-//                             </div>
-//   );
-// }
+      </div>
+      <Authenticator>
+                              {({ signOut, user }) => (
+                                          <main>
+                                            <h1>Hello {user.username}</h1>
+                                            <button onClick={signOut}>Sign out</button>
+                                          </main>
+                              )} 
+                            </Authenticator>
+                            </div>
+  );
+}
 
-// export default withAuthenticator(App);
+export default withAuthenticator(App);
 
 
 // import logo from "./logo.svg";
